85e305e2aad712790f36ca2816e39aaa6c99e9da
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-14 20:15:53 +0000

    setting up count users

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -4,11 +4,8 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const uuidv4 = require('uuid/v4');
 
-// Set the port to 3001
 const PORT = /*process.env.port ||*/ 3001;
 const app = express ();
-
-// Create a new express server
 const server = app.listen(PORT, () => {
   console.log(`WebSocket Server Running on Port: ${PORT}`);
 })
@@ -16,6 +13,7 @@ const server = app.listen(PORT, () => {
 const wss = new SocketServer({ server });
 
 //const clientList = {};
+//--->alt way
 // totalclients += 1;
 // connetClient(weClient,totalClients);
 
@@ -27,24 +25,41 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
+
+const sendConnectedClients = (numclients) => {
+  const clientsconnected = {
+  numcon : numclients,
+  type : "numclients"
+  }  
+  console.log(clientsconnected);
+  wss.broadcast(JSON.stringify(clientsconnected));
+}
+
+//// creating an object, not accessing the values inside - so don't confuse with syntax below
+
 wss.on('connection', (ws) => {
-  //console.log('Client connected');
-//(WsClient, wss.clients.size)
+  //console.log(wss.clients.size);
+  let numconnected = wss.clients.size;
+  sendConnectedClients(numconnected);
 
   ws.on('message', message => { //could rename the message, event... as it's a client event
     console.log(message);//printing sent value to the terminal
     const clientMessage = JSON.parse(message);
+    
+    //where is the best place to capture this number?
     switch(clientMessage.type) {
       case "incomingMessage":
         // handle incoming message
         clientMessage.id = uuidv4();
         clientMessage.type = "postMessage"; //-----this overwrites the type that was sent by the client and re-assigns it 
+        //clientMessage.numcon = numconnected;
         wss.broadcast(JSON.stringify(clientMessage));
       break;
       case "postNotification":
         clientMessage.type = 'incomingNotification'; 
         clientMessage.content = `${clientMessage.oldname} changed their name to ${clientMessage.newname} `; 
         wss.broadcast(JSON.stringify(clientMessage));
+        //clientMessage.numcon = 13;
         break;
       //case "userCount": 
       default:
